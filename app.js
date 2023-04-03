226a5987b7bfbf29bee2e2fc9fd471f41d6de6b4
Shubham Berkar <Sberkar12@gmail.com>
2023-04-03 18:16:47 +0530

    vercel json added

--- app.js
+++ app.js
@@ -1,40 +1,50 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-const kaamRouter = require("./routes/kaams")
-const userRouter = require("./routes/users")
-const refreshRouter = require("./routes/refresh")
+const kaamRouter = require("./routes/kaams");
+const userRouter = require("./routes/users");
+const refreshRouter = require("./routes/refresh");
 
-require("dotenv").config()
-const app = express()
+require("dotenv").config();
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-const allowedOrigins = ["http://localhost:5173"]
+const allowedOrigins = ["http://localhost:5173"];
 
-app.use(cors({
+app.use(
+  cors({
     origin: allowedOrigins,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use("/api/kaam", kaamRouter)
-app.use("/api/u", userRouter)
-app.use("/api/refresh", refreshRouter)
+app.use("/api/kaam", kaamRouter);
+app.use("/api/u", userRouter);
+app.use("/api/refresh", refreshRouter);
+app.use("*", (req, res) => {
+  res.json({
+    message: "no route matched",
+  });
+});
 
 app.use((req, res, next) => {
-    res.status(404).json({
-        message: "route not found"
-    })
-})
+  res.status(404).json({
+    message: "route not found",
+  });
+});
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    console.log("db connected")
-    app.listen(8000)
-}).catch(err => {
-    throw err
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("db connected");
+    app.listen(8000);
+  })
+  .catch((err) => {
+    throw err;
+  });
